14b6eed2219ad5f4a1723936443f90d2fdca02bb
Vasiliy Kirichenko <131616200+VaskaDaGama26@users.noreply.github.com>
2025-11-10 23:38:33 +0300

    fix: fixed package and vite-confog for proper publishing;

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "woop-ui-kit-kva",
-      fileName: (format) => `woop-ui-kit-kva.${format}.js`,
+      name: "woop-ui-kit",
+      fileName: (format) => `woop-ui-kit.${format}.js`,
     },
     rollupOptions: {
       external: ["react"],
